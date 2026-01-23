6cc1d03ab47b9f1f776fa9a72cee516f77d00a29
Demi god <moneshram4@gmail.com>
2026-01-23 16:11:20 +0530

    day 4

---
+++ operators_05.java
@@ -0,0 +1,38 @@
+public class operators_05 {
+    public static void main(String[] args){
+        // arithmetic operators
+
+//        int x = 10;
+//        int y = 2;
+//        int z;
+
+        //z = x + y;
+        // z = x - y;
+        //z= x * y;
+        //z = x / y;
+        //z = x % y;
+
+        //System.out.println(z);
+
+        // Augmented operator
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        //x %= y;
+
+        //System.out.println(x);
+
+        // Increment operator or Decrement operator
+        int x =0;
+//        x = x + 1;
+//        x += 1;
+//        x ++;
+//        x--;
+//        System.out.println(x);
+
+        double result = 9 + 6 / (8 ^ 2) -2;
+        System.out.println(result);
+    }
+}
